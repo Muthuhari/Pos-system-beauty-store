d398c1fa68b655a601d30b4b0fa899aaab40c03d
MUthuhari <86571394+Muthuhari@users.noreply.github.com>
2025-08-22 22:56:54 +0530

    update frontend with login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,36 +1,28 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import ProductCard from "./components/productCard";
+import AdminPage from "./pages/adminPage";
+import HomePage from "./pages/homePage";
+import TestPage from "./pages/test";
+import LoginPage from "./pages/loginPage";
+import { Toaster } from "react-hot-toast";
 
 function App() {
 	return (
-		<>
-			<div className="h-[700px] w-[700px] relative  border-[5px] flex justify-center items-center">
-				<button className="bg-green-900 absolute top-[0px] right-[0px]">
-					X
-				</button>
+		<BrowserRouter>
+			<div className="w-full h-[100vh] ">
+				<Toaster position="top-right"/>
+				<Routes path="/">
+					<Route path="/*" element={<HomePage/>}/>
+					<Route path="/register" element={<h1>Register page</h1>} />
+					<Route path="/login" element={<LoginPage/>} />
+					<Route path="/admin/*" element={<AdminPage/>} />
+					<Route path="/test" element={<TestPage/>}/>			
+				</Routes>
 
-				<div className="w-[300px] h-[100px] relative bg-blue-400 flex justify-center items-center">
-					<button className="bg-red-900 absolute top-[0px] right-[0px]">
-						X
-					</button>
-					<button className="text-white bg-green-500 fixed bottom-[0px] right-[0px] p-[20px]">
-						Chat with whatsapp
-					</button>
-
-					<h1>Your time has over</h1>
-				</div>
-
-        <div className="w-[300px] h-[300px] bg-pink-400 p-[40px] m-[20px]">
-          <div className="w-[50px] h-[50px] bg-yellow-500">
-
-          </div>
-         
-
-        </div>
 			</div>
-		</>
+		</BrowserRouter>
 	);
 }
 
 export default App;
- 
--- frontend/src/components/productCard.jsx
+++ frontend/src/components/productCard.jsx
@@ -1,13 +1,10 @@
-import "./productCard.css";
-export default function ProductCard(props) {
 
-  console.log(props);
+export default function ProductCard(props) {
 
 	return (
 		<div className="productCard">
 			<h1>{props.name}</h1>
 			<p>{props.price}</p>
-      
 			<img
 				className="productImage"
 				src={props.image}
