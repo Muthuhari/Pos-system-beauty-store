ff7dbce10c57d74d116cabdaa855f36bee9533b6
MUthuhari <86571394+Muthuhari@users.noreply.github.com>
2025-08-25 23:54:48 +0530

    added stock attribute

--- backend/models/product.js
+++ backend/models/product.js
@@ -38,7 +38,11 @@ const productSchema = new mongoose.Schema(
   category: {
     type: String,
     required: true
-  },  
+  },
+  stock: {
+      type: Number,
+      required: true,
+      default : 0},  
 })
 
 const Product = mongoose.model("Product", productSchema)
