09e4b9f9501bf716470060458290656de005b2e4
MUthuhari <86571394+Muthuhari@users.noreply.github.com>
2025-09-12 23:24:59 +0530

    update cart section

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,7 @@ import jwt from "jsonwebtoken";
 import productRouter from "./routes/productRouter.js";
 import cors from "cors";
 import dotenv from "dotenv";
+import orderRouter from "./routes/orderRoute.js";
 
 dotenv.config();
 
@@ -56,6 +57,7 @@ mongoose.connect(connectionString).then(
 app.use("/api/students",  studentsRouter);
 app.use("/api/users",  usersRouter );
 app.use("/api/products",  productRouter);
+app.use("/api/orders", orderRouter)
 
 //app.get("/",
 //  (req, res)=>{
