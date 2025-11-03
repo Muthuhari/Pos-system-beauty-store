c19c0e7807d5bf232ba0d0561f22f64adcd7f741
MUthuhari <86571394+Muthuhari@users.noreply.github.com>
2025-11-04 02:46:30 +0530

    added user blocking and forget password

--- backend/routes/usersRouter.js
+++ backend/routes/usersRouter.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {createUser, getUser, googleLogin, loginUser } from "../controllers/UserController.js";
+import {createUser, getUser, googleLogin, loginUser,sendOTP ,getAllUsers,blockOrUnblockUser, changePasswordViaOTP } from "../controllers/UserController.js";
 
 const userRouter = express.Router();
 
@@ -7,5 +7,9 @@ userRouter.post("/",createUser)
 userRouter.post("/login",loginUser)
 userRouter.get("/me",getUser)
 userRouter.post("/google-login",googleLogin)
+userRouter.get("/all-users", getAllUsers)
+userRouter.put("/block/:email",blockOrUnblockUser)
+userRouter.get("/send-otp/:email",sendOTP)
+userRouter.post("/change-password/",changePasswordViaOTP)
 
 export default userRouter;
